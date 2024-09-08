f7f2a6d5afc4c86234949da17fdad455106b9f68
rubencito2002 <ruben.machado1920@gmail.com>
2024-09-08 19:49:47 +0200

    Update CSS styles for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
+// src/app/home/home.component.ts
 import { Component } from '@angular/core';
+import { ProjectsComponent } from '../projects/projects.component';  // Asegúrate de la ruta correcta
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
-  // Tu lógica aquí
+  email = 'ruben.machado1920@gmail.com';
 }
