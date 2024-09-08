770e1703be21acc95ab7ce0c21d14583a763dffb
rubencito2002 <ruben.machado1920@gmail.com>
2024-09-08 19:09:42 +0200

    feat: Agregar componente HomeComponent y estilos CSS actualizados
    Se agregó el componente HomeComponent y se actualizaron los estilos CSS para el componente de inicio. Esto incluye la importación del componente en el archivo app.component.ts y la actualización del archivo app.component.html. También se realizaron cambios en el archivo styles.css para agregar estilos globales y ajustar el tema de color del sitio.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    const lightModeToggler = document.getElementById('lightModeToggler');
-    lightModeToggler?.addEventListener('click', () => {
-      document.body.classList.toggle('light-mode');
-    });
-  }
+export class HomeComponent {
+  // Tu lógica aquí
 }
