bbfea717d588437d3632fede3594ba37a26c736c
rubencito2002 <ruben.machado1920@gmail.com>
2024-09-09 20:31:36 +0200

    Update angular.json, home component HTML, and home component TypeScript files
    Agregar el archivo src/assets/js/script.js a la lista de scripts en el archivo angular.json. En el archivo home.component.html, se agregaron las secciones de proyectos y habilidades. En el archivo home.component.ts, se importó el componente FooterComponent. También se agregó el componente FooterComponent y se actualizó el archivo home.component.html para incluir el componente FooterComponent.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@
 import { Component } from '@angular/core';
 import { ProjectsComponent } from '../projects/projects.component';  // Asegúrate de la ruta correcta
 import { SkillsComponent } from '../skills/skills.component';  // Asegúrate de la ruta correcta
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProjectsComponent, SkillsComponent],
+  imports: [ProjectsComponent, SkillsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
