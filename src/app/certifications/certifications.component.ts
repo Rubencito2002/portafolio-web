e4b6a4852d2318d3e11a2b23771278cfe457f835
rubencito2002 <ruben.machado1920@gmail.com>
2024-09-27 13:03:46 +0200

    Actualizar diseño y estilos del componente Certifications: Ajustar estructura y contenido en certifications.component.html y certifications.component.ts

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -24,60 +24,60 @@ export class CertificationsComponent implements OnInit {
       organization: 'OpenWebinars',
       description: 'Completado el curso de fundamentso de angular.',
       date: 'Julio 2024',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/5QIG?type=png'
     },
     {
       title: 'Python 3 desde cero',
       organization: 'OpenWebinars',
       description: 'Completado el curso de Python desde cero.',
       date: 'Julio 2024',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/A2lM?type=png'
     },
     {
       title: 'Curso de Django',
       organization: 'OpenWebinars',
       description: 'Completado el curso de Django.',
       date: 'Julio 2024',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/Dt89?type=png'
     },
     {
       title: 'Gestion de Formulario en Django',
       organization: 'OpenWebinars',
       description: 'Completado el talle de gestion de formulario en Django.',
       date: 'Noviembre 2024',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/70kb?type=png'
     },
     {
       title: 'Proyecto con Bootstrap y CSS propio',
       organization: 'OpenWebinars',
       description: 'Completado el taller de Bootstrap y CSS propio.',
       date: 'Julio 2024',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/6cWi?type=png'
     },
     {
       title: 'Java 9: Nueva filosofia y funcionalidades',
       organization: 'OpenWebinars',
       description: 'Completado el taller de Java 9.',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/R0GKq?type=png'
     },
     {
       title: 'Introducción a Docker',
       organization: 'OpenWebinars',
       description: 'Completado el curso de Introduccion a Docker.',
       date: 'Octubre 2023',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/SW0r?type=png'
     },
     {
       title: 'Crear un entorno de desarrollo con Docker con Interfaz Grafica',
       organization: 'OpenWebinars',
       description: 'Completado el taller de en crear un entorno de desarrollo con docker.',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/qd7QJ?type=png'
     },
     {
       title: 'Introducción a la Integencia Artificial',
       organization: 'OpenWebinars',
       description: 'Completado el taller sobre IA.',
-      link: 'https://linktocertificate.com'
+      link: 'https://openwebinars.net/certificado/55prd?type=png'
     }
   ];
 
